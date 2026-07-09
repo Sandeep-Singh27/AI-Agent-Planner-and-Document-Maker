7216f52de0a62e64b65d14efa68916c18338355b
Sandeep-Singh27 <sandeepsingh20040927@gmail.com>
2026-07-09 21:00:56 +0530

    Improvement: returns filepath

--- src/generate_document.py
+++ src/generate_document.py
@@ -1,5 +1,9 @@
 from docx import Document
 from src.schema import ExecutionResult
+from pathlib import Path
+
+cwd = Path.cwd()
+output_folder = cwd/"generated"
 
 def generate_docx(
         result: ExecutionResult,
@@ -8,10 +12,12 @@ def generate_docx(
     doc = Document()
 
     for section in result.document:
-        doc.add_heading(section.heading)
+        doc.add_heading(section.heading, level=1)
 
         for paragraph in section.paragraphs:
             if paragraph.strip():
                 doc.add_paragraph(paragraph)
-
+    
+    filename = output_folder/filename
     doc.save(filename)
+    return filename
