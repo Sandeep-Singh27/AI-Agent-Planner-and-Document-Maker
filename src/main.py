6a179280b579dd35a803296393dd153d79050140
Sandeep-Singh27 <sandeepsingh20040927@gmail.com>
2026-07-09 21:01:35 +0530

    Feature: /agent endpoint

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from src.schema import UserRequest
+from src.llm import plan,execute
+from src.generate_document import generate_docx
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 @app.post("/agent")
-def handle_request(request:UserRequest):
-    
+def handle_request(user_request:UserRequest):
+    try:
+        planning = plan(user_request.request)
+        execution_result = execute(plan=planning)
+        file_name = planning.plan_name + ".docx"
+        file_path = generate_docx(execution_result,file_name)
+        return FileResponse(file_path)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Server Side Error")
